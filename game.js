80d9bf11824f31c0a0088fe38d1d65e05dcd01ae
Jade168 <128027202+Jade168@users.noreply.github.com>
2026-02-20 16:39:30 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,4 @@
-// ===== GAME.JS =====
-// V3 CORE - 穩定啟動版
-
-function startGame() {
-
-  // 初始化 3D 引擎
-  initEngine("gameContainer");
-
-  // 載入世界（建築 + 動物）
-  loadWorld();
-
+function startGame(){
+initEngine("gameContainer");
+loadWorld();
 }
